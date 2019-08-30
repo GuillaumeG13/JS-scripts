c8a366cab2f1229a141476e944f0053cb8d7bd49
GAY <guillaume.gay@capgemini.com>
2019-08-30 15:05:21 +0200

    ajout de fonctionnalites

--- src/App.js
+++ src/App.js
@@ -1,49 +1,98 @@
 import React, { Component } from 'react'
 import './App.css'
 import axios from 'axios'
+import {Progress} from 'reactstrap';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
       this.state = {
-        selectedFile: null
+        selectedFile: null,
+        loaded: 0
       }
    
   }
 
+  checkMimeType=(event)=>{
+    //getting file object
+    let files = event.target.files 
+    //define message container
+    let err = ''
+    var x = 0
+    // list allow mime type
+    const types = ['image/png', 'image/jpeg', 'image/gif', 'video/mp4']
+    // loop access array
+    for(x; x<files.length; x++) {
+    // compare file type find doesn't matach
+    // eslint-disable-next-line
+        if (types.every(type => files[x].type !== type)) {
+        // create error message and assign to container   
+        err += files[x].type+' is not a supported format\n';
+        }
+      };
+  
+      if (err !== ''){
+        event.target.value = null 
+        toast.error(err)
+      }
+      
+
+    return true;
+    }
+
   onChangeHandler=event=>{
-    console.log(event.target.files[0])
-    this.setState({
-      selectedFile: event.target.files[0],
-      loaded: 0,
-    })
-}
+    var files = event.target.files[0]
+    if(this.checkMimeType(event)){ 
+    // if return true allow to setState
+      console.log("all right")
+      this.setState({
+      selectedFile: files,
+      loaded: 0
+      })
+    }
+  }
 
   onClickHandler = () => {
     const data = new FormData()
     data.append('file', this.state.selectedFile)
     axios.post("http://localhost:8000/upload", data, { 
+      onUploadProgress: ProgressEvent => {
+        this.setState({
+          loaded: (ProgressEvent.loaded / ProgressEvent.total*100)
+        })}
       // receive two    parameter endpoint url ,form data
-    }).then(res => { // then print response status
-    console.log(res.statusText)
-    })
-    }
+    }).then(res => { 
+      toast.success('upload success')
+  })
+  .catch(err => { 
+      toast.error('upload fail')
+  })
+    
+  }
 
   render() {
     return (
-    
-    <div class="container">
-	    <div class="row">
-	      <div class="col-md-6">
+    <div className="form-group">
+      <ToastContainer />
+    <div className="container">
+	    <div className="row">
+	      <div className="col-md-6">
 	        <form method="post" action="#" id="#">
-            <div class="form-group files">
+            <div className="form-group files">
                 <label>Upload Your File </label>
-                <input type="file" class="form-control" name="file" multiple="" onChange={this.onChangeHandler}></input>
-                <button type="button" class="btn btn-success btn-block" onClick={this.onClickHandler}>Upload</button>             </div>
+                <input type="file" className="form-control" name="file" multiple="" onChange={this.onChangeHandler}></input> 
+            </div>
+            <div className="form-group">
+          <Progress max="100" color="success" value={this.state.loaded} >{Math.round(this.state.loaded,2) }%</Progress>
+          </div>
+          <button type="button" className="btn btn-success btn-block" onClick={this.onClickHandler}>Upload</button>
           </form>
         </div>
 	    </div>
+  </div>
   </div>
     )
   }
